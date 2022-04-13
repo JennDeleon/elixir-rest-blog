4969108880847573af269b2a8b3c72d87270712f
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-04-13 11:26:56 -0500

    adding post controller

--- src/main/java/com/example/restblog/web/PostController.java
+++ src/main/java/com/example/restblog/web/PostController.java
@@ -1,7 +1,12 @@
 package com.example.restblog.web;
 
+import com.example.restblog.data.Post;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 public class PostController {
     @RestController
@@ -9,4 +14,16 @@ public class PostController {
     public class PostsController {
 
     }
+
+    @GetMapping
+    private List<Post> getAll() {
+        return null;
+    }
+
+//    GET /API/POSTS/5 <--- FETCH THE BLOG POST WITH ID 5
+    @GetMapping("{postId}")
+    public Post getById(@PathVariable Long postId){
+        return null;
+    }
+
 }
