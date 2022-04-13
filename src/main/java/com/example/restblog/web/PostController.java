f62fdc805f7ee6f5afec67d3e0e2d707e3fad159
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-04-13 11:50:55 -0500

    adding to PostController

--- src/main/java/com/example/restblog/web/PostController.java
+++ src/main/java/com/example/restblog/web/PostController.java
@@ -6,24 +6,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
+@RestController
+@RequestMapping(value = "/api/posts", headers = "Accept=application/json")
 
 public class PostController {
-    @RestController
-    @RequestMapping(value = "/api/posts", headers = "Accept=application/json")
-    public class PostsController {
 
-    }
 
     @GetMapping
     private List<Post> getAll() {
-        return null;
+        ArrayList<Post> posts = new ArrayList<>();
+        posts.add(new Post (1L, "Post 1 ", "Blah"));
+        posts.add(new Post (2L, "Post 1 ", "Blah Blah"));
+        posts.add(new Post (3L, "Post 1 ", "Blah Blah Blah"));
+
+        return posts;
     }
 
-//    GET /API/POSTS/5 <--- FETCH THE BLOG POST WITH ID 5
+//    GET /API/POSTS/5 <--- EXAMPLE FETCH THE BLOG POST WITH ID 5
     @GetMapping("{postId}")
     public Post getById(@PathVariable Long postId){
-        return null;
+        return new Post(postId, "Post " + postId, "Blah blah new blah");
     }
 
 }
