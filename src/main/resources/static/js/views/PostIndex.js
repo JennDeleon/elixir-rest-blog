15991c51b2057ed1b64b7db124916b977b236ab4
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-04-14 15:25:27 -0500

    working on post index

--- src/main/resources/static/js/views/PostIndex.js
+++ src/main/resources/static/js/views/PostIndex.js
@@ -1,16 +1,52 @@
 export default function PostIndex(props) {
     return `
+<div class="conatiner-fluid">
         <header>
             <h1>Posts Page</h1>
         </header>
         <main>
             <div id="posts-container">
-            ${props.posts.map(post =>{
-                   return `<h3>${post.title}</h3> 
-                   <p>${post.conent}</p>`;
-            }).join('')}   
+            ${props.posts.map(post => {
+        return `<h3>${post.title}</h3> 
+                   <p>${post.content}</p>`;
+    }).join('')}   
             </div>
             
-        </main>
+            
+            <div id="add-post-container">
+                <form id="add-post-form">
+                    <div class="mb-3">
+                        <label for="add-post-title" class="form-label">Title</label>
+                        <input type="text" class="form-control" id="add-post-title" placeholder="Enter title">
+                    </div>
+                    
+                    <div class="mb-3">
+                        <label for="add-post-content" class="form-label">Content</label>
+                        <textarea class="form-control" id="add-post-content" rows="3" placeholder="Enter content"></textarea>
+                    </div>
+                        <button type="button" class="btn btn-primary mb-3"> Save post</button>
+                   </form>
+                </div>
+           </main>
+        </div>
     `;
+}
+
+export function PostEvent(){
+    createAddPostListener();
+
+
+}
+
+function createAddPostListener(){
+    console.log("adding add post listener");
+    $("#add-post-button").click(function () {
+        const title = $("#add-post-title").val();
+        const content = $("#add-post-content").val();
+        const newPost = {
+            title,
+            content
+        }
+
+    });
 }
