905d0d8261aa2c6b57d8ab703d12087a2013a22f
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-04-20 09:36:23 -0500

    working with Post and Post controller

--- src/main/java/com/example/restblog/data/Post.java
+++ src/main/java/com/example/restblog/data/Post.java
@@ -1,17 +1,26 @@
 package com.example.restblog.data;
 
 import lombok.*;
-import java.time.LocalDate;
+
+import javax.persistence.*;
+import java.util.Collection;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
+@Entity
+@Table(name = "posts")
 
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String title;
+    @Column(nullable = false)
     private String content;
-//    private LocalDate createPost;
+//    private User author;
+//    private Collection<Category> categories;
 }
--- src/main/java/com/example/restblog/data/User.java
+++ src/main/java/com/example/restblog/data/User.java
@@ -1,15 +1,24 @@
-//package com.example.restblog.data;
-//
-//import lombok.*;
-//
-//
-//@Getter
-//@Setter
-//@ToString
-//@NoArgsConstructor
-//@AllArgsConstructor
-//
-//public class User {
-//}
+package com.example.restblog.data;
 
+import lombok.*;
 
+import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    public enum Role {USER, ADMIN}
+
+    Long id;
+    String username;
+    String email;
+    String password;
+    LocalDate createdAt;
+    Role role;
+    Collection<Post> posts;
+}
