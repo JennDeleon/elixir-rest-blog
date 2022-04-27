139b718fa3fe2bf8a90e8197de8ae5d49a509667
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-04-26 23:24:09 -0500

    messing with animation

--- src/main/resources/static/js/views/Home.js
+++ src/main/resources/static/js/views/Home.js
@@ -1,25 +1,46 @@
 export default function Home(props) {
     return `
-<head>    <link rel="stylesheet" href="../../style.css"><title></title>
+<head>    
+<link rel="stylesheet" href="../../style.css"><title></title>
 </head>
         <header>
             <h1>WELCOME TO MY BLOG. <br> TO LOGIN, YOU MUST ANSWER THE RIDDLE</h1>
         </header>
-        <main>
-            <div>
-                
-                A man goes to work one day to discover that he could not log on to his computer. His password won't work. Then he remembers that the passwords are reset every month for security reasons. So he goes to his boss's office and had this conversation:
-                <br>
-                Man: "Hey, boss, my password is out of date."
-                <br>
-                Boss: "Yes. The password is different, but if you listen carefully you can figure out the new password. It has the
-                <br>
-                same number of letters as your old one, but only 4 of the letters are the same."
-                Man: "Thanks, boss!"
-                <br>
-                The man then goes to his computer and logs on successfully. Question: What is his new password, and what was the old one?
-                    
+        <main class="riddle">
+        
+            <div class="p1">
+                A man goes to work one day to discover that he could not log on to his computer.
             </div>
+            <div class="p2"> 
+
+                 His password won't work. Then he remembers that the passwords are reset every month for
+            </div>
+            <div class="p3">
+                security reasons. So he goes to his boss's office and had this conversation:
+            </div>
+            <div class="p4">
+            Man: "Hey, boss, my password is out of date."
+           </div>
+                <br>
+           <div class="p5">
+                Boss: "Yes. The password is different, but if you listen carefully
+           </div>
+           <div class="p6">
+                 you can figure out the new password. It has the
+           </div>
+           <div class="p7">
+                same number of letters as your old one, but only 4 of the letters are the same."
+                </div>
+                <div class="p8">
+                Man: "Thanks, boss!"
+           </div>
+                <br>
+           <div class="p9">
+                The man then goes to his computer and logs on successfully. 
+           </div>   
+           <div class="p10">
+           Question: What is his new password, and what was the old one?
+</div>
         </main>
     `;
 }
